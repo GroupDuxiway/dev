cbff38c74c63e1a1144fc217eff8c4e5b88e109d
Manuhutu Nino-Martin <nino.manuhutu@pmu.fr>
2016-04-12 19:47:33 +0200

    Commit

---
+++ setdirs.bat
@@ -0,0 +1,49 @@
+
+
+# create environement de dev
+
+mkdir %1
+cd %1
+ 
+mkdir servers
+mkdir servers\out
+
+mkdir servers\resto
+mkdir servers\resto\app
+mkdir servers\resto\database
+mkdir servers\resto\tests
+
+mkdir servers\beauty
+mkdir servers\beauty\app
+mkdir servers\beauty\database
+mkdir servers\beauty\tests
+
+mkdir servers\wallet
+mkdir servers\wallet\app
+mkdir servers\wallet\database
+mkdir servers\wallet\tests
+
+mkdir apps
+mkdir apps\out
+
+mkdir apps\resto
+mkdir apps\resto\master
+mkdir apps\resto\master\app
+mkdir apps\resto\master\tests
+mkdir apps\resto\user
+mkdir apps\resto\user\app
+mkdir apps\resto\user\tests
+
+mkdir apps\beauty
+mkdir apps\beauty\master
+mkdir apps\beauty\master\app
+mkdir apps\beauty\master\tests
+
+mkdir apps\beauty\user
+mkdir apps\beauty\user\app
+mkdir apps\beauty\user\tests
+
+mkdir apps\wallet
+mkdir apps\wallet\user
+mkdir apps\wallet\user\app
+mkdir apps\wallet\user\tests
---
+++ setenv.bat
@@ -0,0 +1,4 @@
+
+
+set MONGODB=D:\software\MongoDB\Server\3.2\bin
+
