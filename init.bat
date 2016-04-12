e89f037a086ced25c0cc8d6c2043caa392f7dbb5
Manuhutu Nino-Martin <nino.manuhutu@pmu.fr>
2016-04-12 22:44:44 +0200

    Signed-off-by: Manuhutu Nino-Martin

---
+++ init.bat
@@ -0,0 +1,40 @@
+
+mkdir %1\servers\resto\app
+mkdir %1\servers\resto\database
+mkdir %1\servers\resto\tests
+
+
+
+
+mkdir %1\servers\beauty\app
+mkdir %1\servers\beauty\database
+mkdir %1\servers\wallet\tests
+
+
+
+
+
+mkdir %1\apps\resto\master\app
+mkdir %1\apps\resto\master\tests
+
+
+
+
+mkdir %1\apps\resto\user\app
+mkdir %1\apps\resto\user\tests
+
+
+
+
+mkdir %1\apps\beauty\master\app
+mkdir %1\apps\beauty\master\tests
+
+
+
+mkdir %1\apps\beauty\user\app
+mkdir %1\apps\beauty\user\tests
+
+
+
+mkdir %1\apps\wallet\user\app
+mkdir %1\apps\wallet\user\tests
