f602c5deac8d7985e8cba44898b0c1fa63ce5b1b
Manuhutu Nino-Martin <nino.manuhutu@pmu.fr>
2016-04-12 22:04:48 +0200

    Signed-off-by: Manuhutu Nino-Martin

--- setdirs.bat
+++ setdirs.bat
@@ -1,49 +1,66 @@
+@echo off
 
 
-# create environement de dev
+REM create environement de dev
 
 mkdir %1
-cd %1
+
  
-mkdir servers
-mkdir servers\out
+mkdir %1\servers
+mkdir %1\servers\out
 
-mkdir servers\resto
-mkdir servers\resto\app
-mkdir servers\resto\database
-mkdir servers\resto\tests
 
-mkdir servers\beauty
-mkdir servers\beauty\app
-mkdir servers\beauty\database
-mkdir servers\beauty\tests
+git clone https://github.com/GroupDuxiway/serverResto.git %1\servers\resto
+REM  mkdir servers\resto
+REM mkdir servers\resto\app
+REM mkdir servers\resto\database
+REM mkdir servers\resto\tests
 
-mkdir servers\wallet
-mkdir servers\wallet\app
-mkdir servers\wallet\database
-mkdir servers\wallet\tests
+git clone https://github.com/GroupDuxiway/serverBeauty.git %1\servers\beauty
+REM mkdir servers\beauty
+REM mkdir servers\beauty\app
+REM mkdir servers\beauty\database
+REM mkdir servers\beauty\tests
 
-mkdir apps
-mkdir apps\out
+git clone https://github.com/GroupDuxiway/serverWallet.git %1\servers\wallet
+REM mkdir servers\wallet
+REM mkdir servers\wallet\app
+REM mkdir servers\wallet\database
+REM mkdir servers\wallet\tests
 
-mkdir apps\resto
-mkdir apps\resto\master
-mkdir apps\resto\master\app
-mkdir apps\resto\master\tests
-mkdir apps\resto\user
-mkdir apps\resto\user\app
-mkdir apps\resto\user\tests
+mkdir %1\apps
+mkdir %1\apps\out
 
-mkdir apps\beauty
-mkdir apps\beauty\master
-mkdir apps\beauty\master\app
-mkdir apps\beauty\master\tests
 
-mkdir apps\beauty\user
-mkdir apps\beauty\user\app
-mkdir apps\beauty\user\tests
+mkdir %1\apps\resto
 
-mkdir apps\wallet
-mkdir apps\wallet\user
-mkdir apps\wallet\user\app
-mkdir apps\wallet\user\tests
+git clone https://github.com/GroupDuxiway/appsRestoMaster.git %1\apps\resto\master
+REM mkdir apps\resto\master
+REM mkdir apps\resto\master\app
+REM mkdir apps\resto\master\tests
+
+
+git clone https://github.com/GroupDuxiway/appsRestoUser.git %1\apps\resto\user
+REM mkdir apps\resto\user
+REM mkdir apps\resto\user\app
+REM mkdir apps\resto\user\tests
+
+mkdir %1\apps\beauty
+
+git clone https://github.com/GroupDuxiway/appsBeautyMaster.git %1\apps\beauty\master
+REM mkdir apps\beauty\master
+REM mkdir apps\beauty\master\app
+REM mkdir apps\beauty\master\tests
+
+git clone https://github.com/GroupDuxiway/appsBeautyUser.git %1\apps\beauty\user
+REM mkdir apps\beauty\user
+REM mkdir apps\beauty\user\app
+REM mkdir apps\beauty\user\tests
+
+mkdir %1\apps\wallet
+
+git clone https://github.com/GroupDuxiway/appsWalletUser.git %1\apps\wallet\user
+REM mkdir apps\wallet\user
+REM mkdir apps\wallet\user\app
+REM mkdir apps\wallet\user\tests
+
--- setenv.bat
+++ setenv.bat
@@ -1,4 +1,3 @@
 
-
 set MONGODB=D:\software\MongoDB\Server\3.2\bin
 
